160d21dc8c90699a910245c7bd40017c0f60622e
Roman Koshchei <romankoshchei@gmail.com>
2024-11-24 09:57:49 +0200

    put into file

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -1,15 +1,16 @@
+param (
+   [string]$parentFolder,
+   [string]$folder
+)
 
-
-
+Stop-WebSite -Name 'Site'; 
+Set-ItemProperty 'IIS:\\Sites\\Site' -name physicalPath -value $folder; 
+Start-WebSite -Name 'Site';
 
 $directories = (
-   Get-ChildItem -Path '$WEBSITE_PARENT_FOLDER_WIN' -Directory | 
+   Get-ChildItem -Path $parentFolder -Directory | 
    Sort-Object CreationTime -Descending
 );
-
 $latestDirectories = $directories | Select-Object -First 2;
 
-$directories | 
-Where-Object { $_ -notin $latestDirectories } | 
-Remove-Item -Recurse -Force;
-        
+$directories | Where-Object { $_ -notin $latestDirectories } | Remove-Item -Recurse -Force;
